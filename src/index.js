af470ee0305716603cc2565d491233aced15ea34
akibahmed229 <akib4418@gmail.com>
2024-02-01 14:42:01 +0600

    github in action

---
+++ src/index.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+// serving static files
+app.use(express.static("public"));
+
+// route
+app.get("/", (req, res) => {
+  return res.status(200).json({
+    success: true,
+    message: "Hello World!!!",
+  });
+});
+
+// client error
+app.use((req, res, next) => {
+  const error = new Error("Not Found");
+  error.status = 404;
+  next(error);
+});
+
+// server error
+app.use((err, req, res, next) => {
+  res.status(err.status || 500);
+  res.json({
+    error: {
+      status: err.status || 500,
+      message: err.message,
+    },
+  });
+});
+
+// server listening on
+app.listen(port, () => {
+  console.log(`server is running at http://localhost:${port}`);
+});
